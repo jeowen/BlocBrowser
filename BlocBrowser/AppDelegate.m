0c3154f54184fff1423d906fc9594c42af28e2dd
jason.owen@va.gov <jason.owen@va.gov>
2016-01-29 14:54:19 -0800

    commit #2, displays wikipedia

--- BlocBrowser/AppDelegate.m
+++ BlocBrowser/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,16 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    
+    // 1. create a UIWindow object
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+    
+    // 2. add a navigation controller
+    self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[[ViewController alloc] init]];
+    
     return YES;
 }
 
--- BlocBrowser/ViewController.m
+++ BlocBrowser/ViewController.m
@@ -7,21 +7,47 @@
 //
 
 #import "ViewController.h"
+#import <WebKit/WebKit.h>
 
-@interface ViewController ()
 
+@interface ViewController () <WKNavigationDelegate>
+ @property (nonatomic, strong) WKWebView *webView;
 @end
 
+
+
 @implementation ViewController
+- (void)loadView {
+    UIView *mainView = [UIView new];
+    
+    self.webView = [[WKWebView alloc] init];
+    self.webView.navigationDelegate = self;
+    
+    // add a web page to the web view
+    NSString *urlString = @"http://wikipedia.org";
+    NSURL *url = [NSURL URLWithString:urlString];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [self.webView loadRequest:request];
+    
+    NSLog(@"urlstring = %@", urlString);
+    [mainView addSubview:self.webView];
+    
+    
+    self.view = mainView;
+}
 
-- (void)viewDidLoad {
+
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void) viewWillLayoutSubviews {
+    [super viewWillLayoutSubviews];
+    
+    //make the webview fill the main view
+    self.webView.frame = self.view.frame;
+    
 }
-
 @end
